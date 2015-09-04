9067591c8f2a310ebd625d82426b97904ad339c3
Thiago Veronezi <thiago@veronezi.org>
2015-09-04 10:28:13 -0400

    add link to project details page

--- static/assets/app.js
+++ static/assets/app.js
@@ -1,22 +1,26 @@
-var app = angular.module('tribeio', ['ngRoute']);
-app.config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
-    $routeProvider.when('/', {
-        templateUrl: 'app/main.html'
-    }).when('/projects', {
-        templateUrl: 'app/projects.html'
-    }).otherwise({
-        redirectTo: '/'
-    });
-    $locationProvider.html5Mode({
-        enabled: true,
-        requireBase: false
-    });
-}]);
-app.controller('MainController', ['$element', function ($element) {
-    $element.bind('click', function () {
-        $('html, body').animate({scrollTop: $('div.tribe-projects').offset().top}, 'slow');
-    });
-}]);
-app.controller('TwitterController', ['$element', function ($element) {
+angular.module('tribeio', ['ngRoute'])
+    .config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
+        $routeProvider.when('/', {
+            templateUrl: 'app/main.html'
+        }).when('/projects', {
+            templateUrl: 'app/projects.html'
+        }).when('/project-details', {
+            templateUrl: 'app/project-details.html'
+        });
+        $locationProvider.html5Mode({
+            enabled: true,
+            requireBase: false
+        });
+    }])
+    .controller('ProjectsCarousselController', ['$element', '$location', '$scope', function ($element, $location, $scope) {
+        $element.find('article').on('click', function () {
+            $location.url('/project-details');
+            $scope.$apply()
+        });
+    }])
+    .controller('ProjectDetailController', ['$element', function ($element) {
 
-}]);
+    }])
+    .controller('ProjectsController', ['$element', function ($element) {
+
+    }]);
