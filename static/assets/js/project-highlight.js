8b7cdc85dc6296621f11ba3a08ab6c933df3b97e
Thiago Veronezi <thiago@veronezi.org>
2015-09-09 12:46:21 -0400

    asciidoc fix

--- static/assets/js/project-highlight.js
+++ static/assets/js/project-highlight.js
@@ -2,7 +2,9 @@ angular.module('tribe-project-highlight', [])
     .controller('ProjectHighlightController', ['$scope', '$timeout', '$sce', 'tribeProjectsService', function ($scope, $timeout, $sce, tribeProjectsService) {
         tribeProjectsService.onLoad(function (projects) {
             // just grabbing the first project for now TODO
-            $scope.highligtedProject = $sce.trustAsHtml(projects[0].longDescription);
+            var description = projects[0].longDescription;
+            window.console.log(description);
+            $scope.highligtedProject = $sce.trustAsHtml(description);
             $timeout(function () {
                 $scope.$apply();
             }, 0);
