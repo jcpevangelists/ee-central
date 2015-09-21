776d90cc6e099a4a0a8ab66cc1ceba0979007b09
Thiago Veronezi <thiago@veronezi.org>
2015-09-21 14:08:30 -0400

    fixing ui details.

--- src/main/groovy/com/tomitribe/io/www/ServiceTwitter.groovy
+++ src/main/groovy/com/tomitribe/io/www/ServiceTwitter.groovy
@@ -5,11 +5,9 @@ import org.yaml.snakeyaml.Yaml
 import javax.ejb.Lock
 import javax.ejb.LockType
 import javax.ejb.Stateless
-import java.text.SimpleDateFormat
 
 @Stateless
 class ServiceTwitter {
-    private final SimpleDateFormat formatter = new SimpleDateFormat("M/d/yyyy")
 
     @Lock(LockType.READ)
     List<DtoTweet> getTweets() {
@@ -17,7 +15,7 @@ class ServiceTwitter {
             new DtoTweet(
                     text: it.text,
                     author: it.author,
-                    timestamp: formatter.parse(it.date).time
+                    timestamp: Date.parse('M/d/yyyy', it.date as String).time
             )
         }
     }
