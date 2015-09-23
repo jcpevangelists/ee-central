e252a413df01624c814da1147db26ff117b3da9e
Thiago Veronezi <thiago@veronezi.org>
2015-09-23 00:22:48 -0400

    fixing ui details.

--- container_build.sh
+++ container_build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-cd /opt/project/static && npm install
-cd /opt/project/static && gulp build
-cd /opt/project/ && mvn clean install
-chmod -R uao+rwx /opt/project/src/main/webapp/app || true
-chmod -R uao+rwx /opt/project/target/ || true
+mkdir -p /opt/copy/
+cp -R /opt/project/* /opt/copy/
+cd /opt/copy/static && npm install
+cd /opt/copy/static && gulp build
+cd /opt/copy/ && mvn clean install tomee:run
--- run.sh
+++ run.sh
@@ -2,5 +2,5 @@
 docker kill tomitribe-io || true
 docker rm tomitribe-io || true
 docker build -t tomitribe-io .
-docker run --rm -t -i -v $(pwd):/opt/project tomitribe-io
+docker run --rm -t -i -e github_atoken=$github_atoken -v $(pwd):/opt/project:ro -p 8080:8080 tomitribe-io
 
