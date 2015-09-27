9d0bfd4913c395256a99d54013a9f77e6a068f32
Thiago Veronezi <thiago@veronezi.org>
2015-09-26 21:44:58 -0400

    new arrows menu -> footer -> menu -> remove underline on hover

--- static/assets/js/twitter.js
+++ static/assets/js/twitter.js
@@ -9,25 +9,24 @@ angular.module('tribe-twitter', ['tribe-app-service'])
                 }, 0);
                 var position = 0;
                 $scope.tweet = tweets[position];
-                $element.find('i.tribe-left').on('click', function () {
-                    position = position - 1;
-                    if (position < 0) {
-                        position = tweets.length - 1;
+                $scope.onClick = function (dir) {
+                    if ('left' === dir) {
+                        position = position - 1;
+                        if (position < 0) {
+                            position = tweets.length - 1;
+                        }
+                        $scope.tweet = tweets[position];
+                    } else {
+                        position = position + 1;
+                        if (position > tweets.length - 1) {
+                            position = 0;
+                        }
+                        $scope.tweet = tweets[position];
                     }
-                    $scope.tweet = tweets[position];
                     $timeout(function () {
                         $scope.$apply();
                     }, 0);
-                });
-                $element.find('i.tribe-right').on('click', function () {
-                    position = position + 1;
-                    if (position > tweets.length - 1) {
-                        position = 0;
-                    }
-                    $scope.tweet = tweets[position];
-                    $timeout(function () {
-                        $scope.$apply();
-                    }, 0);
-                });
+                };
+
             });
         }]);
