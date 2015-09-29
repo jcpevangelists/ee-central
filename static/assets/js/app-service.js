00d12b90bfa1c92f5bf127d75395dbcf5643d591
Thiago Veronezi <thiago@veronezi.org>
2015-09-29 12:48:17 -0400

    fix bad promise handling

--- static/assets/js/app-service.js
+++ static/assets/js/app-service.js
@@ -1,6 +1,7 @@
 angular.module('tribe-app-service', [])
     .factory('tribeAppService', ['$http', function ($http) {
         var getPromise = $http.get('rest/application');
+        var result = null;
 
         function buildMap(list, key) {
             var map = {};
@@ -20,36 +21,37 @@ angular.module('tribe-app-service', [])
         return {
             whenReady: function (callback) {
                 getPromise.success(function (data) {
-                    var contributors = normalizeArray(data.dtoPage.contributors);
-                    _.each(contributors, function (contributor) {
-                        contributor.contributions = normalizeArray(contributor.contributions);
-                    });
-                    contributors = buildMap(contributors, 'login');
-
-                    var projects = normalizeArray(data.dtoPage.projects);
-                    _.each(projects, function (project) {
-                        project.contributors = _.map(normalizeArray(project.contributors), function (contributor) {
-                            return contributors[contributor.login];
+                    if (!result) {
+                        var contributors = normalizeArray(data.dtoPage.contributors);
+                        _.each(contributors, function (contributor) {
+                            contributor.contributions = normalizeArray(contributor.contributions);
                         });
-                        project.tags = normalizeArray(project.tags);
-                    });
-                    projects = buildMap(projects, 'name');
+                        contributors = buildMap(contributors, 'login');
 
-                    _.each(_.values(contributors), function (contributor) {
-                        _.each(contributor.contributions, function (contribution) {
-                            contribution.project = projects[contribution.project];
+                        var projects = normalizeArray(data.dtoPage.projects);
+                        _.each(projects, function (project) {
+                            project.contributors = _.map(normalizeArray(project.contributors), function (contributor) {
+                                return contributors[contributor.login];
+                            });
+                            project.tags = normalizeArray(project.tags);
                         });
-                    });
+                        projects = buildMap(projects, 'name');
 
-                    var pictures = normalizeArray(data.dtoPage.pictures);
-                    var tweets = normalizeArray(data.dtoPage.tweets);
-
-                    callback({
-                        contributors: contributors,
-                        projects: projects,
-                        pictures: pictures,
-                        tweets: tweets
-                    });
+                        _.each(_.values(contributors), function (contributor) {
+                            _.each(contributor.contributions, function (contribution) {
+                                contribution.project = projects[contribution.project];
+                            });
+                        });
+                        var pictures = normalizeArray(data.dtoPage.pictures);
+                        var tweets = normalizeArray(data.dtoPage.tweets);
+                        result = {
+                            contributors: contributors,
+                            projects: projects,
+                            pictures: pictures,
+                            tweets: tweets
+                        };
+                    }
+                    callback(result);
                 });
             }
         };
