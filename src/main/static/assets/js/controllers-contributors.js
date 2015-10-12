3c21f81ee989afbe6bf8b720eaffb05f93f527aa
Thiago Veronezi <thiago@veronezi.org>
2015-10-12 13:52:34 -0400

    removing unnecessary timeouts

--- src/main/static/assets/js/controllers-contributors.js
+++ src/main/static/assets/js/controllers-contributors.js
@@ -8,36 +8,27 @@
             });
         }])
         .controller('ContributorsListController', [
-            '$scope', '$timeout', 'tribeAppService',
-            function ($scope, $timeout, tribeAppService) {
+            '$scope', 'tribeAppService',
+            function ($scope, tribeAppService) {
                 tribeAppService.whenReady(function (data) {
                     $scope.contributors = _.sortBy(data.contributors, function (contributor) {
                         return -1 * contributor.commits;
                     });
-                    $timeout(function () {
-                        $scope.$apply();
-                    }, 0);
                 });
             }
         ])
         .controller('ContributorsPicturesController', [
-            '$element', '$scope', '$timeout', 'tribeAppService',
-            function ($element, $scope, $timeout, tribeAppService) {
+            '$element', '$scope', 'tribeAppService',
+            function ($element, $scope, tribeAppService) {
                 tribeAppService.whenReady(function (data) {
                     $scope.pictures = _.map(data.pictures, function (pic) {
                         return pic.name;
                     });
                     $scope.selected = $scope.pictures[Math.floor((Math.random() * $scope.pictures.length))];
                     $element.find('.tribe-picture').css('background-image', 'url(rest/images/about/' + $scope.selected + ')');
-                    $timeout(function () {
-                        $scope.$apply();
-                    }, 0);
                     $scope.changePicture = function (pic) {
                         $scope.selected = pic;
                         $element.find('.tribe-picture').css('background-image', 'url(rest/images/about/' + $scope.selected + ')');
-                        $timeout(function () {
-                            $scope.$apply();
-                        }, 0);
                     };
                 });
             }
