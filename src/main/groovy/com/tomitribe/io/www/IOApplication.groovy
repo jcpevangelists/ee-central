2fefc22770f8fadb8479118b126681e180df700b
Romain Manni-Bucau <rmannibu@gmail.com>
2015-10-18 20:21:35 +0200

    renaming ApplicationConfig to be more explicit and match repogistry naming

--- src/main/groovy/com/tomitribe/io/www/IOApplication.groovy
+++ src/main/groovy/com/tomitribe/io/www/IOApplication.groovy
@@ -4,7 +4,7 @@ import javax.ws.rs.ApplicationPath
 import javax.ws.rs.core.Application
 
 @ApplicationPath('/rest')
-class ApplicationConfig extends Application {
+class IOApplication extends Application {
 
 }
 
