ce06d7dd360919094e356b95689ee7572737bfa5
Thiago Veronezi <thiago@veronezi.org>
2015-11-04 13:21:18 -0500

    io toc

--- src/main/static/assets/js/controllers-project-details.js
+++ src/main/static/assets/js/controllers-project-details.js
@@ -8,32 +8,31 @@
             });
         }])
         .controller('ProjectDetailsController', [
-            '$scope', '$routeParams', '$sce', '$templateRequest', 'tribeAppService',
-            function ($scope, $routeParams, $sce, $templateRequest, tribeAppService) {
+            '$scope', '$routeParams', '$sce', '$templateRequest', 'tribeAppService', '$anchorScroll', '$timeout', '$location',
+            function ($scope, $routeParams, $sce, $templateRequest, tribeAppService, $anchorScroll, $timeout, $location) {
 
-                /*
-                function insertShareLinks(originalDocumentation) {
-                    $scope.project.documentation = $sce.trustAsHtml('');
-                    $templateRequest('app/section-share-with-friends-documentation.html').then(function (template) {
-                        var originalEl = angular.element(originalDocumentation);
-                        var originalChildren = originalEl.find('> article > *');
-                        var shareEl = angular.element(template);
-                        var middleEl = angular.element(originalChildren[Math.floor(originalChildren.length / 2)]);
-                        middleEl.after(shareEl);
-                        $scope.project.documentation = $sce.trustAsHtml(originalEl.html());
+                function prepareDocumentation(originalDocumentation) {
+                    var originalEl = angular.element(originalDocumentation);
+                    var toc = originalEl.find('#user-content-toc');
+                    toc.detach();
+                    $scope.project.documentation = $sce.trustAsHtml(originalEl.html());
+                    $timeout(function () {
+                        $anchorScroll($location.hash());
                     });
+                    // uncomment this when implementing https://github.com/tomitribe/tomitribe.io/issues/72
+                    //$templateRequest('app/section-share-with-friends-documentation.html').then(function (template) {
+                    //    var originalChildren = originalEl.find('> article > *');
+                    //    var shareEl = angular.element(template);
+                    //    var middleEl = angular.element(originalChildren[Math.floor(originalChildren.length / 2)]);
+                    //    middleEl.after(shareEl);
+                    //    $scope.project.documentation = $sce.trustAsHtml(originalEl.html());
+                    //});
                 }
-                */
 
                 tribeAppService.whenReady(function (data) {
                     var project = data.projects[$routeParams.project];
                     $scope.project = _.clone(project);
-
-                    // uncomment this when implementing https://github.com/tomitribe/tomitribe.io/issues/72
-                    // insertShareLinks(project.documentation);
-                    // once the line above is uncommented, remove the line below
-                    $scope.project.documentation = $sce.trustAsHtml(project.documentation);
-
+                    prepareDocumentation(project.documentation);
                     $scope.otherProjects = _.filter(_.values(data.projects), function (item) {
                         return item.name !== project.name;
                     });
