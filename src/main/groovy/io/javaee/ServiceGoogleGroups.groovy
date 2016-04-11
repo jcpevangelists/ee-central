2f0a89033fd2d54cf6a096999e8face5fb623d28
Thiago Veronezi <thiago@veronezi.org>
2016-04-11 16:54:39 -0400

    frontend side list google groups messages background

--- src/main/groovy/io/javaee/ServiceGoogleGroups.groovy
+++ src/main/groovy/io/javaee/ServiceGoogleGroups.groovy
@@ -1,17 +1,27 @@
 package io.javaee
 
-import javax.ejb.Stateless
+import javax.annotation.PostConstruct
+import javax.ejb.Lock
+import javax.ejb.LockType
+import javax.ejb.Schedule
+import javax.ejb.Singleton
 import java.nio.charset.StandardCharsets
 
-@Stateless
+@Singleton
+@Lock(LockType.READ)
 class ServiceGoogleGroups {
 
     private String url = 'https://groups.google.com/forum/feed/javaee-guardians/topics/atom.xml?num=50'
 
-    Collection<DtoGroupMessage> listMessages() {
+    Collection<DtoGroupMessage> messages
+
+    @Schedule(hour = "*")
+    @Lock(LockType.WRITE)
+    @PostConstruct
+    void load() {
         String xmlText = url.toURL().getText([:], StandardCharsets.UTF_8.name())
         def xml = new XmlSlurper().parseText(xmlText)
-        return xml.entry.collect {
+        this.messages = xml.entry.collect {
             return new DtoGroupMessage(
                     id: it.id.text(),
                     author: it.author.text(),
@@ -20,6 +30,10 @@ class ServiceGoogleGroups {
                     summary: it.summary.text()
             )
         }
+    }
+
+    Collection<DtoGroupMessage> listMessages() {
+        return this.messages
     }
 }
 
