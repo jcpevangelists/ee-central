aeb1d811304dfe40d511d6b71dc6b43f1d8770c1
Thiago Veronezi <thiago@veronezi.org>
2016-04-19 12:38:26 -0400

    floating menu

--- src/main/static/assets/scripts/header.ts
+++ src/main/static/assets/scripts/header.ts
@@ -17,11 +17,24 @@ angular.module('javaeeio-header', [])
                 }
                 var page = angular.element($window);
                 var background = el.find('.background');
+                var header = el.find('.head');
+                var headerSpacing = el.find('.head-spacing');
                 page.scroll(function () {
                     var step = page.scrollTop();
                     background.css({
                         'transform': 'translateY(' + (step / 3) + 'px)'
                     });
+                    // header out
+                    var heightValue = header.outerHeight();
+                    if (heightValue < step) {
+                        if (!header.hasClass('floating')) {
+                            headerSpacing.css('height', heightValue + 'px');
+                            header.addClass('floating');
+                        }
+                    } else {
+                        headerSpacing.css('height', '');
+                        header.removeClass('floating');
+                    }
                 });
             }
         };
