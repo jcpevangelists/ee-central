f1a81be859ab0963e5a6a5ec5d5708dcbe5a73c8
Thiago Veronezi <thiago@veronezi.org>
2016-04-19 07:20:39 -0400

    get confgiration from system property

--- src/main/groovy/io/javaee/ServiceConfiguration.groovy
+++ src/main/groovy/io/javaee/ServiceConfiguration.groovy
@@ -28,16 +28,16 @@ class ServiceConfiguration implements ConfigurationObserver {
         }
         setIfEmpty('google_forum_url', 'https://groups.google.com/forum/feed/javaee-guardians/topics/atom.xml?num=50')
         setIfEmpty('javaeeio_config_root', {
-            def result = System.getenv()['javaeeio_config_root']
+            def result = System.getProperty('javaeeio_config_root')
             if (!result) {
                 return 'jcpevangelists/javaee.io.config'
             }
             return result
         })
-        setIfEmpty('github_atoken', System.getenv()['github_atoken'])
-        setIfEmpty('javaeeio_twitter_oauth_consumer_key', System.getenv()['javaeeio_twitter_oauth_consumer_key'])
-        setIfEmpty('javaeeio_twitter_oauth_consumer_secret', System.getenv()['javaeeio_twitter_oauth_consumer_secret'])
-        setIfEmpty('javaeeio_twitter_oauth_access_token', System.getenv()['javaeeio_twitter_oauth_access_token'])
-        setIfEmpty('javaeeio_twitter_oauth_access_token_secret', System.getenv()['javaeeio_twitter_oauth_access_token_secret'])
+        setIfEmpty('github_atoken', System.getProperty('javaeeio_github_atoken'))
+        setIfEmpty('javaeeio_twitter_oauth_consumer_key', System.getProperty('javaeeio_twitter_oauth_consumer_key'))
+        setIfEmpty('javaeeio_twitter_oauth_consumer_secret', System.getProperty('javaeeio_twitter_oauth_consumer_secret'))
+        setIfEmpty('javaeeio_twitter_oauth_access_token', System.getProperty('javaeeio_twitter_oauth_access_token'))
+        setIfEmpty('javaeeio_twitter_oauth_access_token_secret', System.getProperty('javaeeio_twitter_oauth_access_token_secret'))
     }
 }
