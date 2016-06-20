1e8a9f9c995511fef1937da6e193d61998406711
Thiago Veronezi <thiago@veronezi.org>
2016-06-20 14:29:23 -0400

    if the value is empty, the property key is not added.

--- src/main/groovy/io/javaee/ServiceConfiguration.groovy
+++ src/main/groovy/io/javaee/ServiceConfiguration.groovy
@@ -39,6 +39,6 @@ class ServiceConfiguration implements ConfigurationObserver {
         // set defaults if values not set already
         setIfEmpty('google_forum_url', 'https://groups.google.com/forum/feed/javaee-guardians/topics/atom.xml?num=50')
         setIfEmpty('javaeeio_config_root', 'jcpevangelists/javaee.io.config')
-        setIfEmpty('devel_pages_root', '')
+        setIfEmpty('devel_pages_root', ' ')
     }
 }
